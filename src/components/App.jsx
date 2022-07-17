596eab333412429c5d11cc2be36f4e1841c13eff
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-07-18 00:16:51 +0800

    feature: add step progress control

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './Header';
 import StepProgress from './StepProgress';
 import Step1 from './Step1';
@@ -5,17 +6,50 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 import ProgressControl from './ProgressControl';
 import Cart from './Cart';
+import Footer from './Footer';
 
 const App = () => {
+  const [step, setStep] = React.useState(1);
+  const [next, setNext] = React.useState('→ 下一步');
+
+  React.useEffect(() => {
+    if (step === 3) {
+      setNext('確認下單');
+    } else {
+      setNext('→ 下一步');
+    }
+  }, [step]);
+
+  const downStep = () => {
+    if (step > 1) setStep((prev) => prev - 1);
+  };
+  const upStep = () => {
+    if (step < 3) setStep((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
       <Header />
-      <StepProgress />
-      <Step1 />
-      <Step2 />
-      <Step3 />
+      <StepProgress nowStep={step} />
+      {step === 1 && <Step1 />}
+      {step === 2 && <Step2 />}
+      {step === 3 && <Step3 />}
+      {step !== 1 && (
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={downStep}
+        >
+          ← 上一步
+        </button>
+      )}
+
+      <button type="button" className="btn btn-danger" onClick={upStep}>
+        {next}
+      </button>
       <ProgressControl />
       <Cart />
+      <Footer />
     </div>
   );
 };
--- src/components/StepProgress/index.js
+++ src/components/StepProgress/index.js
@@ -1,31 +1,106 @@
 import './stepProgress.scss';
 import React from 'react';
 
+const cssSetting = [
+  {
+    step_1: 1,
+    step_1_Css: 'step',
+    step_item_1_css: '',
+    line_1: 'line',
+    step_2: 2,
+    step_2_Css: 'stepDisable',
+    step_item_2_css: 'stepItemDisable',
+    line_2: 'lineDisable',
+    step_3: 3,
+    step_3_Css: 'stepDisable',
+    step_item_3_css: 'stepItemDisable',
+  },
+  {
+    step_1: '✔',
+    step_1_Css: 'stepFinish',
+    step_item_1_css: '',
+    line_1: 'line',
+    step_2: 2,
+    step_2_Css: 'step',
+    step_item_2_css: '',
+    line_2: 'line',
+    step_3: 3,
+    step_3_Css: 'stepDisable',
+    step_item_3_css: 'stepItemDisable',
+  },
+  {
+    step_1: '✔',
+    step_1_Css: 'stepFinish',
+    step_item_1_css: '',
+    line_1: 'line',
+    step_2: '✔',
+    step_2_Css: 'stepFinish',
+    step_item_2_css: '',
+    line_2: 'line',
+    step_3: 3,
+    step_3_Css: 'step',
+    step_item_3_css: '',
+  },
+];
+
 type StepItemProps = {
   step: number,
   name: string,
+  stepCss: string,
+  itemCss: string,
 };
 
 const StepItem: React.FC<StepItemProps> = (props) => {
-  const { step, name } = props;
+  const { step, name, stepCss, itemCss } = props;
   return (
-    <div className="d-flex justify-content-start m-3 ">
-      <div className="step">{step}</div>
+    <div className={`d-flex justify-content-start m-3 ${itemCss}`}>
+      <div className={stepCss}>{step}</div>
       <div>{name}</div>
     </div>
   );
 };
 
-const StepProgress = () => {
+const StepProgress = (props) => {
+  const { nowStep } = props;
+  const [css, setCss] = React.useState(cssSetting[0]);
+  React.useEffect(() => {
+    switch (nowStep) {
+      case 1:
+        setCss(cssSetting[0]);
+        break;
+      case 2:
+        setCss(cssSetting[1]);
+        break;
+      case 3:
+        setCss(cssSetting[2]);
+        break;
+      default:
+    }
+  }, [nowStep]);
   return (
     <>
       <h4 className="m-3">結帳</h4>
       <div className="d-flex justify-content-start align-items-center">
-        <StepItem step={1} name="寄送地址" />
-        <div className="line" />
-        <StepItem step={2} name="運送方式" />
-        <div className="line" />
-        <StepItem step={3} name="付款資訊" />
+        <StepItem
+          step={css.step_1}
+          name="寄送地址"
+          stepCss={css.step_1_Css}
+          itemCss={css.step_item_1_css}
+        />
+        <div className={css.line_1} />
+        <StepItem
+          step={css.step_2}
+          name="運送方式"
+          stepCss={css.step_2_Css}
+          itemCss={css.step_item_2_css}
+        />
+        <div className={css.line_2} />
+        <StepItem
+          step={css.step_3}
+          name="付款資訊"
+          stepCss={css.step_3_Css}
+          itemCss={css.step_item_3_css}
+        />
       </div>
     </>
   );
